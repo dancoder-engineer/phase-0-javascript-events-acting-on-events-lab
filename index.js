b10435b1759b2059f8533f65bc48afc95da5cff5
Dan Seminara <japaneseforgaming@gmail.com>
2021-08-05 11:42:18 -0400

    Assignment Complete!

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+let dodger = document.getElementById('dodger');
+dodger.style.backgroundColor="#FF69B4";
+let leftNumbers = parseInt(dodger.style.left.replace("px", ""));
+let howFast = 5;
+
+function moveDodgerLeft() { 
+    leftNumbers -= 5
+    dodger.style.left = `${leftNumbers}px`; }
+
+function moveDodgerRight() { 
+    leftNumbers += 5
+    dodger.style.left = `${leftNumbers}px`; }
+
+document.addEventListener("keydown", function(e) {
+    //console.log(leftNumbers);
+    if (e.key === 'ArrowRight') { 
+        moveDodgerLeft(); 
+    }
+    if (e.key === 'ArrowLeft') { 
+        moveDodgerRight();
+    }
+  });
